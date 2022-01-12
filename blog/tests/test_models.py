228cfe2eb72e74b19aaf975f5c31a8100968da75
dennis <dennisoseitimss1@gmail.com>
2022-01-11 21:26:06 -0500

    Added tests for models

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -8,4 +8,18 @@ class PostModelTests(TestCase):
     def setUp(self):
         user = User.objects.create(username="Dennis123")
         Post.objects.create(
-        author=user, title="Sample Title", text="Sample Text")
+            author=user,
+            title="Sample Title",
+            text="Sample Text")
+
+    def test_post_title(self):
+        post = Post.objects.get(title="Sample Title")
+        self.assertEquals(f'{post.title}', "Sample Title")
+
+    def test_post_content(self):
+        post = Post.objects.get(title="Sample Title")
+        self.assertEquals(f"{post.text}", "Sample Text")
+
+    def test_post_string_representation(self):
+        post = Post.objects.get(title="Sample Title")
+        self.assertEquals(str(post), f"{post.title}")
